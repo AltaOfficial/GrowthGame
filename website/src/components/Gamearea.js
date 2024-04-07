94d187d2e88865fae6c1804919b58053328c6d17
jaedon farr <jaedonfarr@gmail.com>
2024-04-07 12:56:39 -0400

    Update Gamearea.js

--- website/src/components/Gamearea.js
+++ website/src/components/Gamearea.js
@@ -4,9 +4,7 @@ const Gamearea = () => {
     return ( 
     <div className="gamearea">
         <div className="screen bg-white min-w-svh min-h-svh ml-10 mr-10 rounded-3xl scale-75">
-            <iframe src="./nahhhh/index.html" frameborder="0" className="min-w-full min-h-svh rounded-3xl">
-
-            </iframe>
+            <iframe src="https://itch.io/embed/2631195" href="https://kanodecat.itch.io/growth-game" frameborder="0" className="min-w-full min-h-svh rounded-3xl"></iframe>
         </div>
     </div> 
     );
