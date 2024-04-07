95c0339985c0a7be7eedc897124a4e18e90afca2
jaedon farr <jaedonfarr@gmail.com>
2024-04-07 11:33:03 -0400

    Added Iframe

--- website/src/components/Gamearea.js
+++ website/src/components/Gamearea.js
@@ -1,8 +1,12 @@
+
+
 const Gamearea = () => {
     return ( 
     <div className="gamearea">
         <div className="screen bg-white min-w-svh min-h-svh ml-10 mr-10 rounded-3xl scale-75">
+            <iframe src="./nahhhh/index.html" frameborder="0" className="min-w-full min-h-svh rounded-3xl">
 
+            </iframe>
         </div>
     </div> 
     );
