875f02f422504da30b216ae80b0454399752c49c
jaedon farr <jaedonfarr@gmail.com>
2024-04-14 02:23:08 -0400

    Minor fixes

--- website/src/components/Gamearea.js
+++ website/src/components/Gamearea.js
@@ -4,7 +4,7 @@ const Gamearea = () => {
     return ( 
     <div className="gamearea">
         <div className="screen bg-white min-w-svh min-h-svh ml-10 mr-10 rounded-3xl scale-75">
-            <iframe src="https://itch.io/embed/2631195" href="https://kanodecat.itch.io/growth-game" frameborder="0" className="min-w-full min-h-svh rounded-3xl"></iframe>
+            <iframe title="game-iframe" src="https://itch.io/embed/2631195" href="https://kanodecat.itch.io/growth-game" className="min-w-full min-h-svh rounded-3xl"></iframe>
         </div>
     </div> 
     );
--- website/src/components/Homenavbar.js
+++ website/src/components/Homenavbar.js
@@ -1,7 +1,15 @@
 import Pageicon from "./Pageicon";
 import { Button, Link } from '@radix-ui/themes';
+import { useNavigate } from 'react-router-dom';
 
 const Homenavbar = () => {
+    const navigate = useNavigate();
+    const handleClick = (event) => {
+        if(event.target.className.includes("nav-play-now-cta")){    
+            navigate("/game");
+        }
+    }
+
     return (         
     <div className="navbar grid grid-cols-3 items-center">
         <div className="logo flex items-center mx-auto">
@@ -9,11 +17,11 @@ const Homenavbar = () => {
             <a href="/" className="text-xl"><span className="font-bold">Growth</span> Game</a>
         </div>
         <div className="links space-x-4 mx-auto" highContrast>
-                <Link highContrast underline="none" href="/" className="about hover:underline">About</Link>
+                <Link highContrast underline="none" href="/" className="about">About</Link>
                 <Link highContrast underline="none" href="/" className="how-to-play">How To Play</Link>
         </div>
         <div className="play-now-cta font-Montserrat mx-auto">
-            <Link href='/game'><Button highContrast radius='full'  className="nav-play-now-cta p-4">Play Now</Button></Link>
+            <Button onClick={(e) => {handleClick(e)}} highContrast radius='full'  className="nav-play-now-cta p-4 hover:cursor-pointer">Play Now</Button>
         </div>
     </div>
 );
